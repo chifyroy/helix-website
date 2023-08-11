f495d4160e95f67b837ff193c8e041c96bb4122f
chifyroy <30906899+chifyroy@users.noreply.github.com>
2023-08-11 19:24:18 +0530

    Add files via upload

---
+++ blocks/gallery/gallery.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.gallery').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.gallerythumbnail'
+      });
+      $('.gallerythumbnail').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.gallery',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
+})
