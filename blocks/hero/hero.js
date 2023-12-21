76c377c852cf47e6d595b7249c82505aeb967beb
chifyroy <30906899+chifyroy@users.noreply.github.com>
2023-12-21 11:26:32 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
-  function loadVideo() {
-     $(".hero div div picture").remove();
-     $(".hero div div").append("<video width='100%' height='100%' style='position: absolute; top: 0; left: 0;' autoplay controls><source src='https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4' type='video/mp4'></video>");
-  }
-  loadVideo();
-})
+$(document).ready(function(){
+    var video = $('<video />', {
+        id: 'video',
+        src: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
+        type: 'video/mp4',
+        autoplay: true,
+        muted: true
+    });
+
+    $(".hero div div picture").remove();
+    $(".hero div div").append(video);
+
+    $("video[autoplay]").each(function(){ 
+    this.muted = true;
+        this.play(); 
+    });
+});
