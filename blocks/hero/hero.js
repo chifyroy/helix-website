e1ab0be31baf437cb4722cab8b0eeb2ab4ba92f4
chifyroy <30906899+chifyroy@users.noreply.github.com>
2023-12-21 11:49:28 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
         src: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
         type: 'video/mp4',
         autoplay: true,
-        muted: true
+        muted: true,
+        loop: true
     });
 
     $(".hero div div picture").remove();
